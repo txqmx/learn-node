689c302e8dc5e11970300d48639219361115af5e
liusinan <liusinan@meicai.cn>
2019-09-20 22:29:59 +0800

    安全处理

--- blog-node/src/controller/user.js
+++ blog-node/src/controller/user.js
@@ -1,7 +1,14 @@
-const { exec } = require('../db/mysql' )
+const { exec, escape } = require('../db/mysql' )
+const { genPassword } = require('../utils/cryp')
 
 const login = (userName, password) => {
-    const sql = `select username, realname from users where username='${userName}' and password='${password}'`
+    userName = escape(userName)  // 防止sql注入
+
+    // 生成加密密码
+    password = genPassword(password)
+    password = escape(password)
+
+    const sql = `select username, realname from users where username=${userName} and password=${password}`
     return exec(sql).then(rows => {
         return rows[0] || {}
     })
